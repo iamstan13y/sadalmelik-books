0dba8a9c1278ed8b2b59480cd363e9129b9e3faf
iamstan13y <iamstan13y@gmail.com>
2021-12-26 15:25:22 +0200

    Added Books Controller.

---
+++ sadalmelik-books/Controllers/BooksController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sadalmelik_books.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+
+    }
+}

0d53b36accc40234532b519cf80338cef898dca0
iamstan13y <iamstan13y@gmail.com>
2021-12-27 22:05:24 +0200

    Global Error Handling.

---
+++ sadalmelik-books/Exceptions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using sadalmelik_books.Data.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace sadalmelik_books.Exceptions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static void ConfigureBuildInExceptionHandler(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(appError =>
+            {
+                appError.Run(async context =>
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.OK;
+                    context.Response.ContentType = "application/json";
+
+                    var contextFeature = context.Features.Get<IExceptionHandlerFeature>();
+                    var contextRequest = context.Features.Get<IHttpRequestFeature>();
+
+                    if (contextFeature != null)
+                    {
+                        await context.Response.WriteAsync(new ErrorVM()
+                        {
+                            StatusCode = context.Response.StatusCode,
+                            Message = contextFeature.Error.Message,
+                            Path = contextRequest.Path
+                        }.ToString());
+
+                    }
+                });
+            });
+        }
+    }
+}
--- sadalmelik-books/Startup.cs
+++ sadalmelik-books/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using sadalmelik_books.Data;
 using sadalmelik_books.Data.Services;
+using sadalmelik_books.Exceptions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -59,6 +60,8 @@ namespace sadalmelik_books
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.ConfigureBuildInExceptionHandler();
 
             app.UseEndpoints(endpoints =>
             {

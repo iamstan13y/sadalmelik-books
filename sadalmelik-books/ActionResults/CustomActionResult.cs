4b6ea82c6d72c39f26fef160d01f53df78a8da7f
iamstan13y <iamstan13y@gmail.com>
2021-12-28 14:58:44 +0200

    Clean-up in ActionResults.

--- sadalmelik-books/ActionResults/CustomActionResult.cs
+++ sadalmelik-books/ActionResults/CustomActionResult.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using sadalmelik_books.Data.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace sadalmelik_books.ActionResults

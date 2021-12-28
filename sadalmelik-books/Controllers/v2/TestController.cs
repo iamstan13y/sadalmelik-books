c66cefff1855779795b25ae2b96fa3f9a9bfa92d
iamstan13y <iamstan13y@gmail.com>
2021-12-28 14:11:48 +0200

    URL Based Versioning.

--- sadalmelik-books/Controllers/v2/TestController.cs
+++ sadalmelik-books/Controllers/v2/TestController.cs
@@ -8,12 +8,13 @@ using System.Threading.Tasks;
 namespace sadalmelik_books.Controllers.v2
 {
     [ApiVersion("2.0")]
-    [Route("api/[controller]")]
+    //[Route("api/[controller]")]
+    [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
     public class TestController : ControllerBase
     {
         [HttpGet("get-test-data")]
-        `public IActionResult Get()
+        public IActionResult Get()
         {
             return Ok("Shit 2");
         }

cf3ccb6eb655543d8b2b729717678ab94843a758
iamstan13y <iamstan13y@gmail.com>
2021-12-28 15:06:15 +0200

    Clean up in Data->Models.

--- sadalmelik-books/Data/Models/Book_Author.cs
+++ sadalmelik-books/Data/Models/Book_Author.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace sadalmelik_books.Data.Models
+﻿namespace sadalmelik_books.Data.Models
 {
     public class Book_Author
     {

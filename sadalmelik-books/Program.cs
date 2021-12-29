d9d1cf004ae8db38ae4dc370f8d9caf87ba39a8b
iamstan13y <iamstan13y@gmail.com>
2021-12-29 11:47:33 +0200

    Logging to a File using Serilog - III.

--- sadalmelik-books/Program.cs
+++ sadalmelik-books/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Serilog;
 
@@ -10,9 +11,17 @@ namespace sadalmelik_books
         {
             try
             {
+                var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
                 Log.Logger = new LoggerConfiguration()
-                    .WriteTo.File("Logs/logs.txt", rollingInterval: RollingInterval.Day)
+                    .ReadFrom.Configuration(configuration)
                     .CreateLogger();
+
+                //Log.Logger = new LoggerConfiguration()
+                //    .WriteTo.File("Logs/logs.txt", rollingInterval: RollingInterval.Day)
+                //    .CreateLogger();
 
                 CreateHostBuilder(args).Build().Run();
             }

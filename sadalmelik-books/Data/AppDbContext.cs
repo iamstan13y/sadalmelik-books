21ccd553bd7b1729e2d1b041a1e2d8281c63dac9
iamstan13y <iamstan13y@gmail.com>
2021-12-30 11:08:09 +0200

    Added new Log Table using EFCore Migrations.

--- sadalmelik-books/Data/AppDbContext.cs
+++ sadalmelik-books/Data/AppDbContext.cs
@@ -21,11 +21,15 @@ namespace sadalmelik_books.Data
                 .HasOne(b => b.Book)
                 .WithMany(ba => ba.Book_Authors)
                 .HasForeignKey(bi => bi.BookId);
+
+            modelBuilder.Entity<Log>()
+                .HasKey(n => n.Id);
         }
 
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
         public DbSet<Book_Author> Book_Authors { get; set; }
         public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Log> Logs { get; set; }
     }
 }
--- sadalmelik-books/Migrations/20211230090645_AddLogTable.Designer.cs
+++ sadalmelik-books/Migrations/20211230090645_AddLogTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sadalmelik_books.Data;
 
 namespace sadalmelik_books.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211230090645_AddLogTable")]
+    partial class AddLogTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

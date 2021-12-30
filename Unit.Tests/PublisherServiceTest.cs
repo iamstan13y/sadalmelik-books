3500da76c2e0e89bf9a6d35503810d889122f63d
iamstan13y <iamstan13y@gmail.com>
2021-12-30 19:34:19 +0200

    Setting Up Unit Testing In-Memory DB.

--- Unit.Tests/PublisherServiceTest.cs
+++ Unit.Tests/PublisherServiceTest.cs
@@ -1,12 +1,135 @@
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
+using sadalmelik_books.Data;
+using sadalmelik_books.Data.Models;
+using System;
+using System.Collections.Generic;
 
 namespace Unit.Tests
 {
     public class PublisherServiceTest
     {
-        [SetUp]
+        public static DbContextOptions<AppDbContext> dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+            .UseInMemoryDatabase(databaseName: "SadalmelikTests")
+            .Options;
+
+        AppDbContext context;
+
+        [OneTimeSetUp]
         public void Setup()
         {
+            context = new(dbContextOptions);
+            context.Database.EnsureCreated();
+
+            SeedDatabase();
+        }
+
+        [OneTimeTearDown]
+        public void CleanUp()
+        {
+            context.Database.EnsureDeleted();
+        }
+
+        private void SeedDatabase()
+        {
+            var publishers = new List<Publisher>()
+            {
+                new Publisher()
+                {
+                    Id = 1,
+                    Name = "Rulan Creative"
+                },
+                
+                new Publisher()
+                {
+                    Id = 2,
+                    Name = "Stanton Digital"
+                },
+
+                new Publisher()
+                {
+                    Id = 3,
+                    Name = "Juliet Designs"
+                }
+            };
+
+            context.Publishers.AddRange(publishers);
+
+            var authors = new List<Author>()
+            {
+                new Author()
+                {
+                    Id = 1,
+                    Fullname = "Keith Stanley"
+                },
+                
+                new Author()
+                {
+                    Id = 2,
+                    Fullname = "Juliet Panashe"
+                }
+            };
+
+            context.Authors.AddRange(authors);
+
+            var books = new List<Book>()
+            {
+                new Book()
+                {
+                    Id = 1,
+                    Title = "Romeo & Juliet",
+                    Description = "A story of love.",
+                    IsRead = false,
+                    DateAdded = DateTime.Now.AddDays(-47),
+                    CoverUrl = "https://....com/",
+                    Genre = "Uthando",
+                    PublisherId = 1,
+                    Rate = 5
+                },
+
+                new Book()
+                {
+                    Id = 2,
+                    Title = "Zibanibani My WIfe",
+                    Description = "Just another story of love.",
+                    IsRead = false,
+                    DateAdded = DateTime.Now.AddDays(-15),
+                    CoverUrl = "https://....com/",
+                    Genre = "Uthando",
+                    PublisherId = 1,
+                    Rate = 4
+                }
+            };
+
+            context.Books.AddRange(books);
+
+            var book_authors = new List<Book_Author>()
+            {
+                new Book_Author()
+                {
+                    Id = 1,
+                    AuthorId = 1,
+                    BookId = 1
+                },
+
+                new Book_Author()
+                {
+                    Id = 2,
+                    AuthorId = 1,
+                    BookId = 2
+                },
+
+                new Book_Author()
+                {
+                    Id = 3,
+                    AuthorId = 2,
+                    BookId = 1
+                }
+            };
+
+            context.Book_Authors.AddRange(book_authors);
+
+            context.SaveChanges();
         }
     }
 }

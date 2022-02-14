7b8d459b6c2406591229dc7e394364087055a4d0
iamstan13y <iamstan13y@gmail.com>
2022-02-14 23:14:46 +0200

    docker container deployment to heroku.

--- sadalmelik-books/Startup.cs
+++ sadalmelik-books/Startup.cs
@@ -54,7 +54,7 @@ namespace sadalmelik_books
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
-            if (env.IsDevelopment())
+            if (env.IsDevelopment() || env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
